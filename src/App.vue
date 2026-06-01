919c56a2c65e84b3c34d773f98376f619eed1dca
rizaayson <123900806+rizaayson@users.noreply.github.com>
2026-06-01 19:00:55 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+  import Navbar from './components/Navbar.vue';
+  import About from './components/About.vue';
+  import Projects from './components/Projects.vue';
+  import Tools from './components/Tools.vue';
+  import Contact from './components/Contact.vue';
+  import FooterComponent from './components/FooterComponent.vue';
+
+</script>
+
+<template>
+  <!-- <h1>Test</h1> -->
+
+  <Navbar />
+  <About />
+  <Projects />
+  <Tools />
+  <Contact />
+  <FooterComponent />
+  
+</template>
+
+<style scoped>
+
+</style>
